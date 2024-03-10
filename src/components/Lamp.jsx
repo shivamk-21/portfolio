a62380b72f74147fc086a05f6a124be24b69ed3a
shivamk-21 <2103shivamk@gmail.com>
2024-03-10 23:01:45 +0530

    Modifications to Lamp

--- src/components/Lamp.jsx
+++ src/components/Lamp.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Lamp from "../assets/Lamp.png";
 import Rope from "../assets/rope.png";
-import Bulb from "../assets/Blub.png";
-import WB from "../assets/WB.png";
+import Bulb from "../assets/bulb.svg";
 import "../styles/App.css";
 const LampComp = () => {
   return (
     <div className="themeChange">
       <img src={Bulb} alt="lamp" className="bulb glow" />
-      <img src={WB} alt="lamp" className="bulb" />
       <img src={Rope} alt="lamp" className="rope" />
       <img src={Lamp} alt="lamp" className="lamp" />
     </div>
