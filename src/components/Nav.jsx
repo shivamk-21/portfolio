0d4c7ae71b7351492795a14409677229adf9d04f
shivamk-21 <2103shivamk@gmail.com>
2024-03-12 21:12:20 +0530

    Fixed Mobile nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 
 const Nav = ({ active }) => {
+  const [showhamburger, setShowHamburger] = useState(false);
+  const handleHamburger = () => {
+    setShowHamburger(!showhamburger);
+  };
   return (
     <>
       <div className="web_nav">
@@ -54,31 +58,87 @@ const Nav = ({ active }) => {
         </div>
       </div>
       <div className="mob_nav">
-        <div className="nav">
-          <div className="dropDown">&equiv;</div>
-          <Link
-            className="item"
-            to={active}
-            spy={true}
-            smooth={true}
-            duration={800}
-          >
-            {active === "home" ? (
-              <b>Home</b>
-            ) : active === "projects" ? (
-              <b>Projects</b>
-            ) : active === "edu" ? (
-              <b>Education</b>
-            ) : active === "exp" ? (
-              <b>Experience</b>
-            ) : active === "skills" ? (
-              <b>Skills</b>
-            ) : (
-              "Unknown"
-            )}
-          </Link>
-        </div>
         <div className="glowBar glow" />
+        <div className="nav">
+          <div className="dropDown" onClick={handleHamburger}>
+            &equiv;
+          </div>
+          {!showhamburger && (
+            <div className="item">
+              {active === "home" ? (
+                <b>Home</b>
+              ) : active === "projects" ? (
+                <b>Projects</b>
+              ) : active === "edu" ? (
+                <b>Education</b>
+              ) : active === "exp" ? (
+                <b>Experience</b>
+              ) : active === "skills" ? (
+                <b>Skills</b>
+              ) : (
+                "Unknown"
+              )}
+            </div>
+          )}
+          {showhamburger && (
+            <div className="hamburger">
+              <Link
+                onClick={() => setShowHamburger(false)}
+                className="item"
+                to="home"
+                spy={true}
+                smooth={true}
+                duration={800}
+              >
+                {active === "home" ? <b>Home</b> : "Home"}
+              </Link>
+              <hr />
+              <Link
+                onClick={() => setShowHamburger(false)}
+                className="item"
+                to="projects"
+                spy={true}
+                smooth={true}
+                duration={800}
+              >
+                {active === "projects" ? <b>Projects</b> : "Projects"}
+              </Link>
+              <hr />
+              <Link
+                onClick={() => setShowHamburger(false)}
+                className="item"
+                to="edu"
+                spy={true}
+                smooth={true}
+                duration={800}
+              >
+                {active === "edu" ? <b>Education</b> : "Education"}
+              </Link>
+              <hr />
+              <Link
+                onClick={() => setShowHamburger(false)}
+                className="item"
+                to="exp"
+                spy={true}
+                smooth={true}
+                duration={800}
+              >
+                {active === "exp" ? <b>Experience</b> : "Experience"}
+              </Link>
+              <hr />
+              <Link
+                onClick={() => setShowHamburger(false)}
+                className="item"
+                to="skills"
+                spy={true}
+                smooth={true}
+                duration={800}
+              >
+                {active === "skills" ? <b>Skills</b> : "Skills"}
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
