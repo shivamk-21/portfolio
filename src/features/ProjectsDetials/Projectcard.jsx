0c02507bfa0b211be55cfcc67fc945c8cbf73870
shivamk-21 <2103shivamk@gmail.com>
2024-03-12 21:13:32 +0530

    Fixed Projects Page

--- src/features/ProjectsDetials/Projectcard.jsx
+++ src/features/ProjectsDetials/Projectcard.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 export const Projectcard = ({ title, description, link, image }) => {
   return (
-    <div className="head">
-      <p>{title}</p>
-
+    <>
+      <div className="head">
+        <p>{title}</p>
+      </div>
       <div className="expBoard">
         <h1>About Project</h1>
         <h3>Project Link : {link}</h3>
         <h3>{description}</h3>
         <img src={image} alt="ProjectLogo" />
       </div>
-    </div>
+    </>
   );
 };
