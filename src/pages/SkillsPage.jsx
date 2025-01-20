ba6cc1ffa6f17f8c965f7316194595bb1277f6e6
shivamk-21 <2103shivamk@gmail.com>
2025-01-20 12:38:02 +0530

    Changed Skills Page

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -1,116 +1,27 @@
 import React from "react";
-import py from "../assets/logos/py.png"
-import git from "../assets/logos/git.png"
-import js from "../assets/logos/js.png"
-import react from "../assets/logos/react.png"
-import figma from "../assets/logos/figma.png"
-import ai from "../assets/logos/ai.png"
-import sql from "../assets/logos/sql.png"
-import ml from "../assets/logos/ml.png"
-
+import py from "../assets/logos/py.png";
+import git from "../assets/logos/git.png";
+import js from "../assets/logos/js.png";
+import react from "../assets/logos/react.png";
+import figma from "../assets/logos/figma.png";
+import ai from "../assets/logos/ai.png";
+import sql from "../assets/logos/sql.png";
+import ml from "../assets/logos/ml.png";
 
 const SkillsPage = () => {
   return (
     <div className="skills">
-      {/* <div className='head'>
-        <p>Skills</p>
-        <hr />
-        <div className='row first'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={python} alt="python" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={tf} alt="tf" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={pytorch} alt="pytorch" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={git} alt="git" />
-          </div>
-        </div>
-        <div className='row second'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={django} alt="django" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={javascript} alt="javascript" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={react} alt="react" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={node} alt="node" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={mongodb} alt="mongodb" />
-          </div>
-        </div>
-        <div className='row third'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={ps} alt="ps" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={ai} alt="ai" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={xd} alt="xd" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={figma} alt="figma" />
-          </div>
-        </div>
+      <p>Skills</p>
 
-        <div className='row_mob first'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={python} alt="python" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={tf} alt="tf" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={pytorch} alt="pytorch" />
-          </div>
-        </div>
-        <div className='row_mob second'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={git} alt="git" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={django} alt="django" />
-          </div>
-        </div>
-        <div className='row_mob third'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={javascript} alt="javascript" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={react} alt="react" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={node} alt="node" />
-          </div>
-        </div>
-        <div className='row_mob fourth'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={mongodb} alt="mongodb" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={ps} alt="ps" />
-          </div>
-        </div>
-        <div className='row_mob fifth'>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={ai} alt="ai" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={xd} alt="xd" />
-          </div>
-          <div className='hexagonal-neomorphic-div'>
-            <img src={figma} alt="figma" />
-          </div>
-        </div>
-      </div> */}
+      <img src={py} alt="python" className="py" />
+      <img src={git} alt="git" className="git" />
+      <img src={js} alt="js" className="js" />
+      <img src={react} alt="react" className="react" />
+      <img src={figma} alt="figma" className="figma" />
+      <img src={ai} alt="ai" className="ai" />
+      <img src={sql} alt="sql" className="sql" />
+      <img src={ml} alt="ml" className="ml" />
+      
     </div>
   );
 };
